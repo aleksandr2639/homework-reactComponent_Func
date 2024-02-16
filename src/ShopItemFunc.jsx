5fc26b63cd03792b491b252c6a127507c2dc039a
aleksandr2639 <alexanderkriulin@yandex.ru>
2024-02-16 17:32:04 +0300

    fix: ShopItemFunc component

--- src/ShopItemFunc.jsx
+++ src/ShopItemFunc.jsx
@@ -1,23 +1,14 @@
-function ShopItemFunc () {
-  const item = {
-    brand: 'Tiger of Sweden',
-    title: 'Leonard coat',
-    description: 'Minimalistic coat in cotton-blend',
-    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-    price: 399,
-    currency: '£'
-  }
-
+function ShopItemFunc (props) {
   return (
     <div className="main-content">
-      <h2>{item.brand}</h2>
-      <h1>{item.title}</h1>
-      <h3>{item.description}</h3>
-      <div className="description">{item.descriptionFull}</div>
+      <h2>{props.item.brand}</h2>
+      <h1>{props.item.title}</h1>
+      <h3>{props.item.description}</h3>
+      <div className="description">{props.item.descriptionFull}</div>
       <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
       <div className="divider"></div>
       <div className="purchase-info">
-        <div className="price">{item.currency}{item.price}.00</div>
+        <div className="price">{props.item.currency}{props.item.price}.00</div>
         <button>Добавить в корзину</button>
       </div>
     </div>
